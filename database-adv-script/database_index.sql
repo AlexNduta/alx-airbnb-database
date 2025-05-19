13f4c950de069a116bc646ba76243f5b5cb3c176
AlexNduta <aleckskin1@gmail.com>
2025-05-19 22:23:15 +0300

    working with indexes and comparing outputs

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -19,3 +19,33 @@ CREATE INDEX idx_property_location ON "Property" (location);
 -- indexes on the start and end dates
 CREATE INDEX idx_booking_start_date ON booking (start_date);
 CREATE INDEX idx_booking_end_date ON booking (end_date);
+
+
+-- measure query perfomance
+-- Before adding an index
+EXPLAIN ANALYZE SELECT property_id, review_id, rating, user_id, rating
+FROM Review WHERE comment = 'Average stay';
+                                               QUERY PLAN                                               
+--------------------------------------------------------------------------------------------------------
+ Seq Scan on review  (cost=0.00..71.10 rows=1320 width=56) (actual time=0.011..0.489 rows=1320 loops=1)
+   Filter: (comment = 'Average stay'::text)
+   Rows Removed by Filter: 1408
+ Planning Time: 0.051 ms
+ Execution Time: 0.558 ms
+(5 rows)
+
+-- after adding an index
+EXPLAIN ANALYZE SELECT property_id, review_id, rating, user_id, rating
+FROM Review WHERE comment = 'Average stay';
+                                               QUERY PLAN
+--------------------------------------------------------------------------------------------------------
+ Seq Scan on review  (cost=0.00..71.10 rows=1320 width=56) (actual time=0.014..0.724 rows=1320 loops=1)
+   Filter: (comment = 'Average stay'::text)
+   Rows Removed by Filter: 1408
+ Planning Time: 0.199 ms
+ Execution Time: 0.828 ms
+(5 rows)
+
+
+-- I honestly did not see much changes, maybe I should try it out on a larger dataset
+
